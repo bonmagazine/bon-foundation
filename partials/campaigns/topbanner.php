2a4b0ff7adeaf5bbad6bacac746cfc9c31ed5504
Afonso Duarte <afonsomartins@gmail.com>
2014-08-19 17:19:55 +0100

    remove debug crap from top banner

--- partials/campaigns/topbanner.php
+++ partials/campaigns/topbanner.php
@@ -3,10 +3,6 @@
 <div class="top-banner">
   <?php foreach ($top_banner as $post): setup_postdata( $post ); ?>
     <?php the_content(); ?>
-    <?php  print_r( get_post_meta( get_the_ID() )); ?>
   <?php endforeach; wp_reset_postdata(); ?>
-
-  <?php echo date("Y-m-d"); ?>
-
 </div>
 <?php endif; ?>
