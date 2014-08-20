df8d6e86254b3b16e9a16bdac0982eae13162236
Afonso Duarte <afonsomartins@gmail.com>
2014-08-20 22:34:18 +0100

    bon poster date bug

--- library/models/landing.php
+++ library/models/landing.php
@@ -35,11 +35,18 @@ function bon_get_poster_posts() {
   $poster_args = array(
     'post_type' => 'bon_posters',
     'posts_per_page' => -1, 
-    'meta_key' => 'end_date',
-    'meta_value' => date("Y/m/d h:i A"),
-    'meta_compare' => '>',
     'orderby' => 'date',
-    'order' => 'DESC'
+    'order' => 'DESC',
+
+    'meta_key' => 'end_date',
+    'meta_query' => array(
+        array(
+            'key' => 'end_date',
+            'value' => current_time( 'mysql' ),
+            'compare' => '>=',
+            'type' => 'date'
+            )
+        )
    );
   return get_posts($poster_args); 
 }
