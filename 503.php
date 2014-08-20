c5f90a8020172f34a89d5c49e5e91be37024181a
Afonso Duarte <afonsomartins@gmail.com>
2014-08-20 13:28:32 +0100

    503 page for maintenance mode

---
+++ 503.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>SITENAME</title>
+
+<link rel="stylesheet" href="//f.fontdeck.com/s/css/usizZWFdN2CyQNkbgHeoeWIoXHw/bon.se/30102.css" type="text/css">
+<style type="text/css">
+
+body {
+	background-image: url(http://dev.bonmagazine.com/live/wp-content/uploads/2011/09/bonse-aug1489.jpg);
+	background-attachment:fixed;
+	margin:0;
+	font-family: "Archive Pro Roman";
+	font-size: 18px;
+}
+h2 {
+	font-size: 28px;	
+	font-weight: normal;
+	margin-bottom: 30px;
+}
+#page {
+	width:1000px;
+	margin:0px auto;
+	background: white;
+	height:100vh;	
+}
+#logo {
+      padding:50px;
+      text-align: center;
+}
+p { margin-bottom:30px; }
+.center {
+       text-align: center;
+}
+.uppercase {text-transform: uppercase;}
+#social-media a {
+       display: inline-block;
+       margin: 15px 5px;
+}
+
+</style>
+
+</head>
+<body>
+<div id="page">
+<div id="logo">
+<img src="http://bon.se/live/wp-content/uploads/2014/08/bon-logo-large.png" alt="Bon Logo">
+</div>
+<h2 class="uppercase center">Snart tillbaka</h2>
+<p class="center">Bon.se uppdateras under dagen och är tillbaka 23.59 ikväll. Ses då!</p>
+<p class="center">
+<a href="https://www.facebook.com/bonmagazine" class=" fb main-nav-link" target="_blank"><img src="http://bon.se/live/wp-content/themes/bon/images/Facebook.png" width="24"></a>&nbsp;&nbsp;
+<a href="https://twitter.com/bonmagazine" class="twitter main-nav-link" target="_blank"><img src="http://bon.se/live/wp-content/themes/bon/images/Twitter.png" width="24"></a>&nbsp;&nbsp;
+<a href="http://instagram.com/bonmagazine" class="instagram main-nav-link" target="_blank"><img src="http://bon.se/live/wp-content/themes/bon/images/Instagram.png" width="24"></a>
+</div>
+
+</body>
+</html>
+
