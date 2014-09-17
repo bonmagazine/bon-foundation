de58f02cb2c9865ba7e511113954da4efd017a18
Afonso Duarte <afonsomartins@gmail.com>
2014-09-17 19:46:11 +0200

    cover ads fix

--- partials/excerpt-cover.php
+++ partials/excerpt-cover.php
@@ -6,8 +6,12 @@
  * @since bon 1.0
  */
 ?>
+<?php if( in_category( array('short-post', 'ad') ) ) $short_post = true; ?>
+<?php if( in_category( array('ad') ) ) $ad_post = true; ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( the_cover_template_class() ); ?>>
+  
+  <?php if( !$ad_post ): ?>
   <header>
     <div class="section">
       <p class="small-sys-title"><?php the_terms( $post->ID, 'section' ); ?></p>
@@ -17,7 +21,11 @@
       <?php bon_the_entry_author(); ?>
     </div>
   </header>
+  <?php endif; ?>
+
   <div class="main">
+
+    <?php if( !$ad_post ): ?>
     <div class="entry-meta">
       <div class="section">
         <p class="small-sys-title"><?php the_terms( $post->ID, 'section' ); ?></p>
@@ -27,20 +35,24 @@
       </div>
       <?php get_template_part( 'partials/socialmedia' ); ?>
     </div>
+    <?php endif; ?>
+    
+    <?php if( !$short_post ): ?>
     <div class="thumbnail">
       <a href="<?php the_permalink(); ?>">
       <?php the_cover_thumbail(); ?>
       </a>
     </div>
+    <?php endif; ?>
+
     <div class="entry-excerpt">
-      <?php if( in_category( array('short-post', 'ad') ) ): ?>
+      <?php if( $short_post ): ?>
         <?php the_content(); ?>
       <?php else: ?>
-        <p>
-          <?php the_excerpt(); ?> 
-          <a class="read-more" href="<?php the_permalink(); ?>">»&nbsp;Läs&nbsp;mer</a>
-        </p>
-        
+      <p>
+        <?php the_excerpt(); ?> 
+        <a class="read-more" href="<?php the_permalink(); ?>">»&nbsp;Läs&nbsp;mer</a>
+      </p>
       <?php endif; ?>
     </div>
   </div>
