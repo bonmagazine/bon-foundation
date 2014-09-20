cebc599d5d4f921b485aede50afaf7b0fb9a55bc
Afonso Duarte <afonsomartins@gmail.com>
2014-09-21 00:44:58 +0200

    bon blogs pages

--- js/app.js
+++ js/app.js
@@ -42,7 +42,7 @@ Bon = function() {
     }
 
     // Sticky header for bon blogs
-    var $bonBlogs = $('body.post-type-archive-bon_blogs,body.single-bon_blogs');
+    var $bonBlogs = $('body.post-type-archive-bon_blogs,body.single-bon_blogs,body.single-bon_blogs_pages');
     if($bonBlogs.length > 0) {
       $sticky = $('.sticky');
       $sticky.waypoint('sticky', {offset: $sticky.css('top')});
