a76f22805e07bdd26dd89206dbedc4ee23d15ae9
Afonso Duarte <afonsomartins@gmail.com>
2014-10-01 18:19:15 +0100

    bon blogs comment count and social buttons everywhere

--- partials/content-bon_blogs.php
+++ partials/content-bon_blogs.php
@@ -11,12 +11,22 @@
 	<header>
 		<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		<?php bon_the_entry_meta(); ?>
-		<span class="comment-count"><a href="<?php echo $item->permalink ?>#disqus_thread"></a></span>
 	</header>
 	<div class="entry-content">
 		<?php the_content(__('Continue reading...', 'bon')); ?>
 	</div>
-	<footer class="entry-tags">
-	  <?php echo get_the_term_list(get_the_ID(), 'bon_blogs_tag', '<p><span class="small-sys-title">Taggar – </span>', ', ', '</p>'); ?>
+	<footer class="entry-footer">
+	  <p class="entry-tags"><span class="small-sys-title">Taggar – </span>
+	  <?php echo get_the_term_list(get_the_ID(), 'bon_blogs_tag', '', ', '); ?>
+	  </p>
+	  <span class="comment-count"><a href="<?php the_permalink();  ?>#disqus_thread"></a></span>
+
+	  <div class="entry-social">
+	    <iframe src="//www.facebook.com/plugins/like.php?href=<?php echo urlencode(get_the_permalink()); ?>&amp;width=50&amp;layout=box_count&amp;action=like&amp;show_faces=false&amp;share=false&amp;height=65&amp;appId=395965657184574" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:50px; height:65px; margin-right: 30px" allowTransparency="true"></iframe>
+
+	    <iframe allowtransparency="true" frameborder="0" scrolling="no"
+	            src="https://platform.twitter.com/widgets/tweet_button.html?count=vertical"
+	            style="width:60px; height:65px;"></iframe>
+	  </div>
 	</footer>
 </article>
--- single-bon_blogs.php
+++ single-bon_blogs.php
@@ -17,13 +17,24 @@
           <?php bon_the_entry_date(); ?>
         </div>
       </header>
+
+      <div class="entry-social">
+        <iframe src="//www.facebook.com/plugins/like.php?href=<?php echo urlencode(get_the_permalink()); ?>&amp;width=50&amp;layout=box_count&amp;action=like&amp;show_faces=false&amp;share=false&amp;height=65&amp;appId=395965657184574" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:50px; height:65px; margin-right: 30px" allowTransparency="true"></iframe>
+
+        <iframe allowtransparency="true" frameborder="0" scrolling="no"
+                src="https://platform.twitter.com/widgets/tweet_button.html?count=vertical"
+                style="width:60px; height:65px;"></iframe>
+      </div>
+
       <div class="entry-content">
         <?php the_content(); ?>
 
         <?php get_template_part( 'partials/campaigns/articleinsert' ); ?>
       </div>
-      <footer class="entry-tags">
-        <?php echo get_the_term_list(get_the_ID(), 'bon_blogs_tag', '<p><span class="small-sys-title">Taggar – </span>', ', ', '</p>'); ?>
+      <footer class="entry-footer">
+        <p><span class="small-sys-title">Taggar – </span>
+        <?php echo get_the_term_list(get_the_ID(), 'bon_blogs_tag', '', ', '); ?>
+        </p>
       </footer>
     </article>
     <?php if(is_single() && comments_open() ): ?>
