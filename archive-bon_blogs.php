4e49c9e0dcd0fc0a16142a2029831d30228fd815
erikhartin <erik@sans-titre.net>
2014-10-21 15:42:59 +0100

    Bon blogs archive design update

--- archive-bon_blogs.php
+++ archive-bon_blogs.php
@@ -11,7 +11,8 @@
   <?php endif; ?>
 
   <?php if ( have_posts() ) : ?>
-    <section id="blog-list" class="bon-blog-list infinite-scroll">
+    <section id="blog-list" class="bon-blog-list archive-blog-list archive-compact infinite-scroll">
+	  <h1 class="archive-title">Arkiv</h2>  
 
       <?php $index_date = new DateTime("+12 months"); ?>
 
@@ -22,13 +23,15 @@
           <?php if ($index_date->format('Y') != $post_date->format('Y')
                 ||  $index_date->format('n') !== $post_date->format('n')): 
                 $index_date = $post_date; ?>
-            <h1 class="hentry year date-separator small-sys-title">
+            <h1 class="hentry year date-separator small-medium-title">
               <?php echo $post_date->format('F'); ?> <?php echo $post_date->format('Y'); ?> 
             </h1>
           <?php endif; ?>
         <?php endif; ?>
-
-        <?php get_template_part( 'partials/content', 'bon_blogs' ); ?>
+		<header>
+		    <time class="the-day" datetime="<?php echo get_the_time('c') ?>" pubdate><?php echo get_the_date('j') ?></time>
+			<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		</header>
       <?php endwhile; ?>
     </section>
     <?php if( get_query_var('yearly') !== 1 ): // hide disqus from yearly archive?>
