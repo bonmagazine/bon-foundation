1a5bc18aeb1bfc6d91719d75a97e6ecf8f3f8be4
Afonso Duarte <afonsomartins@gmail.com>
2015-01-26 16:02:26 +0000

    oembeds

--- library/oembed.php
+++ library/oembed.php
@@ -2,8 +2,12 @@
 
 // Register oEmbed providers
 function bon_oembed_provider() {
-  wp_oembed_add_provider( 'http://bon.se', 'http://bon.se/oembed*', false );
+  wp_oembed_add_provider( 'http://bon.se/*', 'http://bon.se/oembed', false );
 }
-
-// Hook into the 'init' action
 add_action( 'init', 'bon_oembed_provider' );
+
+// Wrap oEmbeds in flex video divs
+function bon_embed_oembed_html($html, $url, $attr, $post_id) {
+  return '<figure class="flex-video">' . $html . '</figure>';
+}
+add_filter('embed_oembed_html', 'bon_embed_oembed_html', 99, 4);
