e3983507606cb2a24b6577b05a36baa20a209b26
Erik Hartin <erik@bonmagazine.com>
2015-11-03 16:35:35 +0000

    Bug fix, fix skip button placement

--- partials/campaigns/overlay.php
+++ partials/campaigns/overlay.php
@@ -1,19 +1,20 @@
 <?php $overlay = bon_get_campaign('overlay'); ?>
 <?php if($overlay): ?>
 	<?php foreach ($overlay as $post): setup_postdata( $post ); ?>
-<div class="campaign-promo-container">
-  <div class="campaign-promo-wrap">
 	<?php 
 	$selected = get_field('show_skip_button');
 	if( in_array('on', $selected) ) :
 	?>
+<div class="campaign-promo-container">
+  <div class="campaign-promo-wrap">
     <div class="btn btn-skip section">
 	    <svg class="close-icon"><use xlink:href="#close-icon" /></svg>
 		<p><?php echo get_field( "skip_button_text" ) ?></p>
 	</div>
-	<? endif ?>
     <?php the_content(); ?>
   </div>
 </div>
+<?php the_content(); ?>
+<?php endif ?>
 	<?php endforeach; wp_reset_postdata(); ?>
 <?php endif; ?>
